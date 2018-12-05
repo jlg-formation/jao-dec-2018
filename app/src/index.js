308417c8a686efcad957a9b0530a74c3c9ee4efa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-05 09:22:05 +0100

    ok

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,10 @@
-console.log('coucou');
+console.log('coucouxxx');
+
+import Hashes from 'jshashes';
+
+const SHA256 =  new Hashes.SHA256;
+
+var str = 'Sample text!'
+
+console.log('SHA256: ' + SHA256.hex(str));
+
