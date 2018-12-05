ab2af2b24e6ec28598b5499dfe6f9a68f6f6ff41
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-05 10:40:34 +0100

    ok

--- app/src/index.js
+++ app/src/index.js
@@ -1,40 +1,50 @@
-console.log('coucou');
+// import Hashes from 'jshashes';
 
-import Hashes from 'jshashes';
-
-const SHA256 = new Hashes.SHA256;
-
-const str = 'Sample text!'
-
-console.log('SHA256: ' + SHA256.hex(str));
+// const SHA256 = new Hashes.SHA256;
+// const str = 'Sample text!'
+// console.log('SHA256: ' + SHA256.hex(str));
 
 
 const cx = 300;
 const cy = 300;
 const r = 270;
-
+const total = 100;
 
 function draw() {
     const body = document.querySelector('body');
-    console.log('body', body);
     body.innerHTML = `
 <svg height="600" width="600">
-    <circle cx="300" cy="300" r="270" stroke="black" stroke-width="3" fill="hsl(0, 100%, 50%, 0.1)" />
+    <circle cx="${cx}" cy="${cy}" r="${r}" stroke="black" stroke-width="3" fill="hsl(0, 100%, 50%, 0.1)" />
 </svg>
     `;
-
-
-    const total = 10;
     for (let i = 0; i < total; i++) {
         addGraduation((360 / total) * i);
     }
 
+    for (let i = 0; i < total; i++) {
+        addLine(i);
+    }
+
+}
+
+function addLine(i) {
+    const angle1 = (2 * Math.PI / total) * i;
+    const angle2 = (2 * Math.PI / total) * i * 2;
+
+    const svg = document.querySelector('svg');
+    const line = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+    line.setAttribute("x1", cx + r * Math.cos(angle1));
+    line.setAttribute("y1", cy + r * Math.sin(angle1));
+    line.setAttribute("x2", cy + r * Math.cos(angle2));
+    line.setAttribute("y2", cy + r * Math.sin(angle2));
+    line.setAttribute("stroke", "black");
+    line.setAttribute("stroke-width", "3");
+    svg.appendChild(line);
 }
 
 function addGraduation(angle) {
     angle = angle * 2 * Math.PI / 360;
     const svg = document.querySelector('svg');
-    console.log('svg', svg);
     const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
     circle.setAttribute("cx", cx + r * Math.cos(angle));
     circle.setAttribute("cy", cy + r * Math.sin(angle));
