ab5fd8bbbc31995473ef3fc1a253ea3c61f01b26
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-05 10:04:21 +0100

    ok

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,22 @@
-console.log('coucouxxx');
+console.log('coucou');
 
 import Hashes from 'jshashes';
 
-const SHA256 =  new Hashes.SHA256;
+const SHA256 = new Hashes.SHA256;
 
 var str = 'Sample text!'
 
 console.log('SHA256: ' + SHA256.hex(str));
 
+
+function draw() {
+    const body = document.querySelector('body');
+    console.log('body', body);
+}
+
+addEventListener('DOMContentLoaded', draw);
+
+/* <svg height="600" width="600">
+        <circle cx="300" cy="300" r="270" stroke="black" stroke-width="3" fill="hsl(0, 100%, 50%, 0.1)" />
+        <circle cx="300" cy="300" r="5" stroke="black" stroke-width="3" />
+    </svg> */
